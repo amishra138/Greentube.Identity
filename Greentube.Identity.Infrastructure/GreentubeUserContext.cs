adad5e81509a2b239266043a3fccb26bf91a3c63
Arvind Mishra <arvind.mishra1991@outlook.com>
2020-05-29 13:48:53 +0530

    Added configuration and Repository in Infrastrucutre library

---
+++ Greentube.Identity.Infrastructure/GreentubeUserContext.cs
@@ -0,0 +1,20 @@
+﻿using Greentube.Identity.Infrastructure.EntityConfigurations;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Greentube.Identity.Infrastructure
+{
+    public class GreentubeUserContext : IdentityDbContext
+    {
+        public GreentubeUserContext(DbContextOptions<GreentubeUserContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new UserEntityConfiguration());
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ Greentube.Identity.Infrastructure/Repository/UserRepository.cs
@@ -0,0 +1,62 @@
+﻿using Greentube.Identity.Domain.Entities;
+using Greentube.Identity.Domain.Interfaces;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Greentube.Identity.Infrastructure.Repository
+{
+    /// <summary>
+    /// User repository contract's implementation
+    /// </summary>
+    public class UserRepository : IUserRepository
+    {
+        private bool _disposed;
+        private readonly UserManager<UserEntity> _userManager;
+
+        public UserRepository(UserManager<UserEntity> userManager)
+        {
+            _userManager = userManager ?? throw new ArgumentNullException(nameof(userManager));
+        }
+
+        public Task<IEnumerable<UserEntity>> GetAll()
+        {
+            //just to wrap into a task
+            return Task.FromResult(_userManager.Users.AsEnumerable());
+        }
+
+        public Task<UserEntity> GetByEmailAsync(string email)
+        {
+            return _userManager.FindByEmailAsync(email);
+        }
+
+        public async Task<bool> ResetPassword(UserEntity user, string token, string newPassword)
+        {
+            var result = await _userManager.ResetPasswordAsync(user, token, newPassword);
+
+            return result == null ? false : result.Succeeded;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+
+            //To inform GC that this object was cleaned up fully, so that it wouldn't waste time cleaning it again
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    _userManager.Dispose();
+                }
+            }
+            _disposed = true;
+        }
+    }
+}
